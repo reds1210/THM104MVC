2df4bbe961400cdd91e298e5fbecf84e72959f68
reds1210 <reds1210@gmail.com>
2024-01-24 14:24:24 +0800

    替換form表單

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -7,6 +8,18 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+
+
+
+
+
+
             return View();
         }
     }
