158b7f53e7a6668c98ef4ccdb69e2aa72034b0ec
reds1210 <reds1210@gmail.com>
2024-01-24 15:31:42 +0800

    增加登入給cookie

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using WebApplication1.Models;
+using WebApplication1.Models.Entity;
 
 namespace WebApplication1.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly NorthwindContext _db;
+
+        public AccountController(NorthwindContext db)
+        {
+            _db = db;
+        }
         [HttpGet]
         public IActionResult Login()
         {
@@ -12,10 +22,31 @@ namespace WebApplication1.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel model)
+        public async IActionResult Login(LoginViewModel model)
         {
+            //去資料庫比對資料
+            var user = _db.Stuednts.FirstOrDefault(x => x.Email == model.Email && x.Password == model.Password);
+            if (user == null)
+            {
+                ViewBag.Error = "帳號密碼錯誤";
+                return View();
+            }
 
+            //做一張憑證
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.Name),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Gender, user.Gender.ToString()),
+                new Claim("userId", user.Id.ToString())
+            };
 
+            var claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal=new ClaimsPrincipal(claimsIdentity);
+
+            //把憑證給他
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,claimsPrincipal);
 
 
 
