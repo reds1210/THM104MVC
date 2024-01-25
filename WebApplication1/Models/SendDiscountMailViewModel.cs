4c5962895941d1b6114d66a6e5044adea2791358
reds1210 <reds1210@gmail.com>
2024-01-25 16:26:04 +0800

    寄信實作

---
+++ WebApplication1/Models/SendDiscountMailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class SendDiscountMailViewModel
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
